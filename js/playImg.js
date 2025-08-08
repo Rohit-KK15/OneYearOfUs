bf8330a2074b42623f86750726174d9602617eec
Keerthikanth Rohit. A <rohitkeerthikanth@gmail.com>
2025-08-08 22:54:01 +0530

    Update playImg.js

--- js/playImg.js
+++ js/playImg.js
@@ -15,6 +15,7 @@ function showImage(){
 }
 
 function play(){
+	document.getElementById("heartTxt").style.display = "none";
 	if(t == 0){
 		myImage.setAttribute("src", "");
 		myTxt.innerHTML = "";
